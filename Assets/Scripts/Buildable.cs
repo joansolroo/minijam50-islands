bb422e9ac3e876560e2bf6c8ae5f864baa272a56
Erwan Normand <normand.erwan@protonmail.com>
2020-03-20 15:30:01 +0100

    Add Cabin

---
+++ Assets/Scripts/Buildable.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class Buildable : MonoBehaviour
+{
+    [SerializeField]
+    private List<BuildableStep> steps;
+
+    [SerializeField]
+    private Wood wood;
+
+    private BuildableStep currentStep;
+
+    public void AddWood(float value)
+    {
+        wood.Add(value);
+
+        ActiveCurrentStep();
+    }
+
+    private void OnValidate()
+    {
+        steps.Clear();
+        foreach (Transform child in transform)
+        {
+            var step = child.GetComponent<BuildableStep>();
+            if (step != null)
+            {
+                step.gameObject.SetActive(false);
+                steps.Add(step);
+            }
+        }
+
+        currentStep = null;
+        ActiveCurrentStep();
+    }
+
+    private void ActiveCurrentStep()
+    {
+        foreach (var step in steps)
+        {
+            if (wood.Value >= step.minValue)
+            {
+                if (currentStep == null)
+                {
+                    SetCurrentStep(step);
+                }
+                else if (step.minValue > currentStep.minValue)
+                {
+                    SetCurrentStep(step);
+                }
+            }
+        }
+    }
+
+    private void SetCurrentStep(BuildableStep step)
+    {
+        if (currentStep != null)
+        {
+            currentStep.gameObject.SetActive(false);
+        }
+
+        step.gameObject.SetActive(true);
+        currentStep = step;
+    }
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -31,6 +31,17 @@ public class PlayerController : MonoBehaviour
         currentBiome = collision.transform.GetComponent<Biome>();
     }
 
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        var buildable = collision.transform.GetComponent<Buildable>();
+        if (buildable != null)
+        {
+            var value = resources.Wood.Value;
+            buildable.AddWood(value);
+            resources.Wood.Remove(value);
+        }
+    }
+
     private void OnCollisionExit2D(Collision2D collision)
     {
         var hitBiome = collision.transform.GetComponent<Biome>();
