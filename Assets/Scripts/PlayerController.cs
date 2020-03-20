ad0a9a1444bc5f7d03c0e0cf1a175ffa3c3f470d
Unknown <titwin@live.fr>
2020-03-20 14:43:44 +0100

    add jumping
    - and minor bug fix

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,27 +5,58 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float speed = 0.1f;
+    public float jumpForce = 10f;
+    public float fallingFriction = 0.05f;
+    public bool jumping = false;
+    public float distToGround;
+
+    private Vector2 velocity;
     private Rigidbody2D body;
-    private AnimationController animation;
+    private AnimationController animationController;
     private Vector3 direction;
 
     private void Start()
     {
+        velocity = Vector2.zero;
         body = GetComponent<Rigidbody2D>();
-        animation = GetComponent<AnimationController>();
+        animationController = GetComponent<AnimationController>();
         direction = Vector3.right;
     }
 
     // Update is called once per frame
     void Update()
     {
-        body.MovePosition(transform.position + Input.GetAxis("Horizontal") * speed * Vector3.right);
+        jumping = !IsGrounded();
+        if (!jumping && Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            body.AddForce(new Vector2(0f, jumpForce));
+            jumping = true;
+        }
+        if(body.velocity == Vector2.zero && jumping)
+        {
+            transform.position -= fallingFriction * Vector3.up;
+        }
+
+        velocity = new Vector2(Input.GetAxis("Horizontal") * speed, body.velocity.y);
+        Vector3 dummy = Vector3.zero;
+        body.velocity = velocity;
+
         if (Input.GetAxis("Horizontal") == 0f)
-            animation.playAnimation(AnimationController.AnimationType.IDLE, direction.x < 0f);
+            animationController.playAnimation(AnimationController.AnimationType.IDLE, direction.x < 0f);
         else
         {
             direction = Input.GetAxis("Horizontal") * Vector3.right;
-            animation.playAnimation(AnimationController.AnimationType.WALKING, direction.x < 0f);
+            animationController.playAnimation(AnimationController.AnimationType.WALKING, direction.x < 0f);
         }
+    }
+
+    private bool IsGrounded()
+    {
+        return Physics2D.Raycast(transform.position, -Vector3.up, distToGround, 1 << LayerMask.NameToLayer("Ground"));
+    }
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = IsGrounded() ? Color.red : Color.white;
+        Gizmos.DrawLine(transform.position, transform.position - Vector3.up * distToGround);
     }
 }
