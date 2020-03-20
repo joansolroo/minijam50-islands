3ae846892fd60abfb2babc3d03399b3db630cd3f
Unknown <titwin@live.fr>
2020-03-20 11:11:25 +0100

    first 3C

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 0.1f;
+    private Rigidbody2D body;
+
+    private void Start()
+    {
+        body = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetAxis("Horizontal") > 0f)
+            body.MovePosition(transform.position + speed * Vector3.right);
+    }
+}
