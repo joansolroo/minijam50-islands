e8b05a37e0e290d8e799616dba7d836fafad831d
Sol Roo <solroo@gmail.com>
2020-03-22 16:17:28 +0100

    giving up on using dittered light

---
+++ Assets/Scripts/PixelPerfectTransform.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PixelPerfectTransform : MonoBehaviour
+{
+    [SerializeField] float posterization = 16;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        Vector3 position = transform.parent.position;
+        position.x = (int)((position.x) * posterization) / posterization;
+        position.y = (int)((position.y) * posterization) / posterization;
+        this.transform.position = position;     
+    }
+}
